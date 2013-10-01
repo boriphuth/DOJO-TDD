498537e6db09c156598d7f8057f1bd7d223b760e
boriphuth <savepoo@hotmail.com>
2013-10-01 18:18:59 +0700

    Lesson 1 DOJO TDD

---
+++ lesson1/Grade/Grade/Grade.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Grade
+{
+    public class Grade
+    {
+        public int grade(int score)
+        {
+            if (score < 0) throw new UnderFlow();
+            if (score >= 80 && score <= 100) return 4;
+            if (score >= 70 && score < 80) return 3;
+            if (score >= 60 && score < 70) return 2;
+            if (score >= 50 && score < 60) return 1;
+            if (score >= 0 && score < 50) return 0;
+            return 0;
+        }
+    }
+
+    public class UnderFlow : FormatException
+    { 
+    
+    }
+}
---
+++ lesson1/Grade/GradeTest/GradeTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Grade;
+
+namespace GradeTest
+{
+    [TestClass]
+    public class GradeTest
+    {
+        private Grade.Grade grade = new Grade.Grade();
+
+        [TestMethod]
+        public void whenScoreIsMoreThan80_thenGradeIs4()
+        {
+            Assert.AreEqual(4, grade.grade(80));
+            Assert.AreEqual(4, grade.grade(81));
+        }
+
+        [TestMethod]
+        public void whenScoreIsMoreThan70_thenGradeIs3()
+        {
+            Assert.AreEqual(3, grade.grade(70));
+            Assert.AreEqual(3, grade.grade(79));
+        }
+
+        [TestMethod]
+        public void whenScoreIsMoreThan60_thenGradeIs2()
+        {
+            Assert.AreEqual(2, grade.grade(60));
+            Assert.AreEqual(2, grade.grade(69));
+        }
+
+        [TestMethod]
+        public void whenScoreIsMoreThan50_thenGradeIs1()
+        {
+            Assert.AreEqual(1, grade.grade(50));
+            Assert.AreEqual(1, grade.grade(59));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(UnderFlow))]
+        public void whenScoreIsLessThan0_thenThrowException()
+        {
+            grade.grade(-1);
+        }
+    }
+}
